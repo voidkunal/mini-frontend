46f5e326b67447e889174bd3a1af307f17dcd04b
voidkunal <kunalmandal1607@gmail.com>
2025-07-02 23:54:22 +0530

    back to basic

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // load .env variables
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-    },
-    define: {
-      __VITE_BACKEND_URL__: JSON.stringify(env.VITE_BACKEND_URL),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Vercel uses this for deployment
+  },
 });
