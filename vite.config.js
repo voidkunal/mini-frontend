9de53aead1664f982f97f23b0af0c3a22dd33a25
voidkunal <kunalmandal1607@gmail.com>
2025-07-02 23:25:04 +0530

    vercel file correct

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,6 +15,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Ensure the build output is directed to the dist folder
   },
 });
