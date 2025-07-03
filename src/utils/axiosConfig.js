5aa465a4aa677a133af16b3951e940eb8455c9a3
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 22:48:13 +0530

    update

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,9 +1,9 @@
-// ✅ axiosConfig.js (fixed version for Vercel + Render)
+// client/src/utils/axiosConfig.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
-  withCredentials: true, // ✅ SEND cookies (important)
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`, 
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
