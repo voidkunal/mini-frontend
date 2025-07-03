91fdfebcf23daa5644e9f3b23163d9b3b1edc0ba
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 23:07:21 +0530

    axios

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,12 +1,8 @@
-// client/src/utils/axiosConfig.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`, 
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1",
+  withCredentials: true, // ✅ send cookies (e.g., token cookie)
 });
 
 export default axiosInstance;
