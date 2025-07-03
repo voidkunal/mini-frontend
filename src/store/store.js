6ad07d7e5e56e5a72954a16d969b140b05ef3d35
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 23:01:01 +0530

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,51 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import authReducer from "./slice/authSlice";
-// import popupReducer from "./slice/popupSlice";
-// import userReducer from "./slice/userSlice";
-// import bookReducer from "./slice/bookSlice";
-// import borrowReducer from "./slice/borrowSlice";
-
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-
-// // ✅ Configuration for redux-persist (only persists auth.user & isAuthenticated)
-// const persistConfig = {
-//   key: "auth",
-//   storage,
-//   whitelist: ["isAuthenticated", "user"],
-// };
-
-// // ✅ Wrap authReducer with persistence
-// const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-
-// // ✅ Store setup
-// export const store = configureStore({
-//   reducer: {
-//     auth: persistedAuthReducer,
-//     popup: popupReducer,
-//     user: userReducer,
-//     book: bookReducer,
-//     borrow: borrowReducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         // ✅ Avoid redux-persist action warnings
-//         ignoredActions: [
-//           "persist/PERSIST",
-//           "persist/REHYDRATE",
-//           "persist/PAUSE",
-//           "persist/FLUSH",
-//           "persist/PURGE",
-//           "persist/REGISTER",
-//         ],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-
-
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import popupReducer from "./slice/popupSlice";
@@ -55,10 +7,11 @@ import borrowReducer from "./slice/borrowSlice";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
+import { Cookie } from "lucide-react";
 
 const persistConfig = {
   key: "auth",
-  storage: storageSession, // ✅ session only, not localStorage
+  storage: Cookie, // ✅ session only, not localStorage
   whitelist: ["isAuthenticated", "user"],
 };
 
