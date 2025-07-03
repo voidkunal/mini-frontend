f49b2e7588a36539a4346c3a4d5ea555e1def2ec
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 12:29:24 +0530

    fixed code

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,11 +1,10 @@
-// frontend/src/utils/axiosConfig.js
 import axios from "axios";
 
 const API = "https://mini-backend-a8ay.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: API,
-  withCredentials: true, // ✅ Required for sending cookies
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
