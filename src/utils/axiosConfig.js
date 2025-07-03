9608a350be73d09d411b246cfcd275f76aaa9945
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 23:52:15 +0530

    fuck

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1",
-  withCredentials: true, // MUST HAVE for cookies to work
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true, // ✅ ensures cookies sent across domains
 });
 
 export default axiosInstance;
