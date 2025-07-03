90d45047a4994fcccf4dfb2a86a0cf19b9f3b315
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 23:02:32 +0530

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,49 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./slice/authSlice";
+// import popupReducer from "./slice/popupSlice";
+// import userReducer from "./slice/userSlice";
+// import bookReducer from "./slice/bookSlice";
+// import borrowReducer from "./slice/borrowSlice";
+
+// import { persistStore, persistReducer } from "redux-persist";
+// import storageSession from "redux-persist/lib/storage/session";
+// import { Cookie } from "lucide-react";
+
+// const persistConfig = {
+//   key: "auth",
+//   storage: Cookie, // ✅ session only, not localStorage
+//   whitelist: ["isAuthenticated", "user"],
+// };
+
+// const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistedAuthReducer,
+//     popup: popupReducer,
+//     user: userReducer,
+//     book: bookReducer,
+//     borrow: borrowReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [
+//           "persist/PERSIST",
+//           "persist/REHYDRATE",
+//           "persist/PAUSE",
+//           "persist/FLUSH",
+//           "persist/PURGE",
+//           "persist/REGISTER",
+//         ],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
+
+// store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import popupReducer from "./slice/popupSlice";
@@ -5,39 +51,12 @@ import userReducer from "./slice/userSlice";
 import bookReducer from "./slice/bookSlice";
 import borrowReducer from "./slice/borrowSlice";
 
-import { persistStore, persistReducer } from "redux-persist";
-import storageSession from "redux-persist/lib/storage/session";
-import { Cookie } from "lucide-react";
-
-const persistConfig = {
-  key: "auth",
-  storage: Cookie, // ✅ session only, not localStorage
-  whitelist: ["isAuthenticated", "user"],
-};
-
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-
 export const store = configureStore({
   reducer: {
-    auth: persistedAuthReducer,
+    auth: authReducer,
     popup: popupReducer,
     user: userReducer,
     book: bookReducer,
     borrow: borrowReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          "persist/PERSIST",
-          "persist/REHYDRATE",
-          "persist/PAUSE",
-          "persist/FLUSH",
-          "persist/PURGE",
-          "persist/REGISTER",
-        ],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
