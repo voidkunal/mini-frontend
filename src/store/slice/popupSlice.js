8bce9df98d46730ca680b8e5851568b13ad1e424
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 23:44:36 +0530

    popup

--- src/store/slice/popupSlice.js
+++ src/store/slice/popupSlice.js
@@ -1,27 +1,60 @@
 // src/store/slice/popupSlice.js
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isPopupOpen: false,
-  popupContent: null,
+  settingPopup: false,
+  addBookPopup: false,
+  readBookPopup: false,
+  recordBookPopup: false,
+  returnBookPopup: false,
+  addNewAdminPopup: false,
 };
 
 const popupSlice = createSlice({
   name: "popup",
   initialState,
   reducers: {
-    openPopup: (state, action) => {
-      state.isPopupOpen = true;
-      state.popupContent = action.payload;
+    toggleSettingPopup: (state) => {
+      state.settingPopup = !state.settingPopup;
     },
-    closePopup: (state) => {
-      state.isPopupOpen = false;
-      state.popupContent = null;
+    toggleAddBookPopup: (state) => {
+      state.addBookPopup = !state.addBookPopup;
+    },
+    toggleReadBookPopup: (state) => {
+      state.readBookPopup = !state.readBookPopup;
+    },
+    toggleRecordBookPopup: (state) => {
+      state.recordBookPopup = !state.recordBookPopup;
+    },
+    toggleReturnBookPopup: (state) => {
+      state.returnBookPopup = !state.returnBookPopup;
+    },
+    toggleAddNewAdminPopup: (state) => {
+      state.addNewAdminPopup = !state.addNewAdminPopup;
+    },
+    closeAllPopup: (state) => {
+      Object.keys(state).forEach((key) => {
+        state[key] = false;
+      });
+    },
+    closePopup: (state, action) => {
+      const popupName = action.payload;
+      if (popupName in state) {
+        state[popupName] = false;
+      }
     },
   },
 });
 
-export const { openPopup, closePopup } = popupSlice.actions;
+export const {
+  toggleSettingPopup,
+  toggleAddBookPopup,
+  toggleReadBookPopup,
+  toggleRecordBookPopup,
+  toggleReturnBookPopup,
+  toggleAddNewAdminPopup,
+  closeAllPopup,
+  closePopup,
+} = popupSlice.actions;
 
 export default popupSlice.reducer;
