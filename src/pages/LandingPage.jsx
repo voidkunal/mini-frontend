ced922260d72018b22004d603a1d296ba1b5ecfe
voidkunal <kunalmandal1607@gmail.com>
2025-07-04 23:53:32 +0530

    Study Era

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -14,20 +14,20 @@ const LandingPage = () => {
         transition={{ duration: 0.8, ease: "easeOut" }}
         className="bg-white/10 backdrop-blur-xl p-10 rounded-3xl shadow-2xl text-center max-w-5xl w-full flex flex-col md:flex-row items-center gap-10"
       >
-        {/* Hero Image */}
+       
         <img
           src={heroImage}
           alt="Hero Visual"
           className="w-full md:w-72 h-auto object-cover rounded-xl shadow-xl"
         />
 
-        {/* Content */}
+        
         <div className="flex-1">
           <h1 className="text-5xl font-extrabold mb-4 text-white drop-shadow-sm">
-            Welcome to <span className="text-blue-300">Void Tech World</span>
+            Welcome to <span className="text-blue-300">Study Era</span>
           </h1>
           <p className="text-lg mb-8 text-gray-200">
-            Your smart library management system base Application for Students.
+          Smart library management system Application for Rural Students.
           </p>
 
           <div className="flex flex-col sm:flex-row justify-center gap-5">
