fb2a12c255f1346ffd1e956ef29c434552a63222
voidkunal <kunalmandal1607@gmail.com>
2025-07-05 12:54:11 +0530

    axios timing fixed

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 50000,
+  timeout: 20000,
 });
 
 axiosInstance.interceptors.request.use(
